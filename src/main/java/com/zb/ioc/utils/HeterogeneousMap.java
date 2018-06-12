ed2b969a6ba92afe7c01bb6c105f8f2f524ea595
DovahYol <DovahYol@live.com>
2018-06-12 10:37:42 +0800

    Naive Bean Annotation Processing generally completed Working on inter-bean reference

--- src/main/java/com/zb/ioc/utils/HeterogeneousMap.java
+++ src/main/java/com/zb/ioc/utils/HeterogeneousMap.java
@@ -4,9 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HeterogeneousMap extends HashMap implements Map {
-    public <T> T getObject(T key) {
+    public <T> T get(Class<T> key) {
         @SuppressWarnings("unchecked")
         T result = (T)super.get(key);
+        return result;
+    }
+
+    public <T> T put(Class<T> key, T value) {
+        @SuppressWarnings("unchecked")
+        T result = (T)super.put(key, value);
         return result;
     }
 }
